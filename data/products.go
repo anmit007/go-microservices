8306d1cd721f1557c09ceaa244820e9b29b4c1aa
Anmit Ray <anmitray@Anmits-Mac-mini.local>
2025-01-15 21:10:36 +0530

    some restapis

--- data/products.go
+++ data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -31,10 +32,44 @@ func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(p)
 }
+func (p *Product) FromJson(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(p)
+
+}
 
 // GetProducts returns a list of products
 func GetProducts() Products {
 	return productList
+}
+func Addproduct(p *Product) {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+func UpdateProducts(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+	p.ID = id
+	productList[pos] = p
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("PRODUCT NOT FOUND")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
+}
+func getNextID() int {
+	lastProd := productList[len(productList)-1]
+	return lastProd.ID + 1
 }
 
 // productList is a hard coded list of products for this
--- handlers/products.go
+++ handlers/products.go
@@ -3,31 +3,102 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/anmit007/data"
 )
 
+// Products is a http.Handler
 type Products struct {
 	l *log.Logger
 }
 
+// NewProducts creates a products handler with the given logger
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
+// ServeHTTP is the main entry point for the handler and staisfies the http.Handler
+// interface
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-
+	// handle the request for a list of products
 	if r.Method == http.MethodGet {
 		p.getProducts(rw, r)
 		return
 	}
-	rw.WriteHeader(http.StatusNotImplemented)
+
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+	if r.Method == http.MethodPut {
+		p.l.Println("PUT")
+		// we want to have id in the uri
+		regex := regexp.MustCompile(`/([0-9]+)`)
+		matches := regex.FindAllStringSubmatch(r.URL.Path, -1)
+		if len(matches) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		if len(matches[0]) != 2 {
+			http.Error(rw, "Invalid URI More than one cg", http.StatusBadRequest)
+			return
+		}
+		idString := matches[0][1]
+		id, _ := strconv.Atoi(idString)
+		p.l.Println("got id", id)
+
+		p.updateProducts(id, rw, r)
+	}
+	// catch all
+	// if no method is satisfied return an error
+	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
-func (p *Products) getProducts(rw http.ResponseWriter, h *http.Request) {
+
+// getProducts returns the products from the data store
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
+
+	// fetch the products from the datastore
 	lp := data.GetProducts()
+
+	// serialize the list to JSON
 	err := lp.ToJSON(rw)
 	if err != nil {
-		http.Error(rw, "unable to unmarshal json", http.StatusInternalServerError)
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJson(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
 	}
 
+	data.Addproduct(prod)
+}
+
+func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJson(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+	err = data.UpdateProducts(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "PRODUCT NOT FOUND", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(rw, "somethinng went wrn", http.StatusNotFound)
+		return
+	}
 }
